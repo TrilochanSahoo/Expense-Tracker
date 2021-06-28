47889eee56f0d9f0aa0c6ac06af846f610cafe5c
TrilochanSahoo <71021905+TrilochanSahoo@users.noreply.github.com>
2021-06-28 19:56:04 +0530

    update : adding expenseDate file

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import ExpanseDate from './ExpenseDate'
 import './ExpenseItem.css'
 
 const ExpenseItem = (props) => {
     return (
     <div className="expense-item">
-        <div >{props.date.toISOString()}</div>
+        <ExpanseDate date={props.date}></ExpanseDate>
         <div className="expense-item-desc">
             <h2>{props.title}</h2>
             <div className="expense-item-price">{props.amount}</div>
