f25f15174a8384e4f9119e1bb85107ac3be455bf
TrilochanSahoo <71021905+TrilochanSahoo@users.noreply.github.com>
2021-06-30 11:08:04 +0530

    feat : adding logic to store input data

--- src/components/newExpense/ExpenseForm.js
+++ src/components/newExpense/ExpenseForm.js
@@ -1,13 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ExpenseForm.css'
 
 const ExpenseForm = ()=>{
+    const [item,setItem] = useState('')
+    const [amount,setAmount] = useState('')
+    const [date,setDate] = useState('')
+    function changeNameHandler (event){
+        setItem(event.target.value)
+        // console.log(item)
+    }
+    function changeAmountHandler (event){
+        setAmount(event.target.value)
+        // console.log(item)
+    }
+    function changeDateHandler (event){
+        setDate(event.target.value)
+        // console.log(item)
+    }
+
+    function submitHandler(event){
+        event.preventDefault()
+        const expenseData = {
+            title : item,
+            amount : amount,
+            date : new Date(date)
+        }
+        console.log(expenseData)
+        setItem('')
+        setAmount('')
+        setDate('')
+    }
     return (
-        <form>
+        <form onSubmit={submitHandler}>
             <div>
-                <input type="text" placeholder="enter item name"></input>
-                <input type="number" min='1' step="1" placeholder="enter item name"></input>
-                <input type="date" min='2019-01-01' max='2022-12-31'></input>
+                <input type="text" placeholder="enter item name" value={item} onChange={changeNameHandler}></input>
+                <input type="number" min='1' step="1" placeholder="enter amount" value={amount} onChange={changeAmountHandler}></input>
+                <input type="date" min='2019-01-01' max='2022-12-31' value={date} onChange={changeDateHandler}></input>
             </div>
             <div>
                 <button>Submit</button>
